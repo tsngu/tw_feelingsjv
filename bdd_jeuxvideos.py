9cf71c697ec5dec90bfa6c732acbfedad65d990b
Clément Buon <aubanel.buon@gmail.com>
2024-03-18 15:36:21 +0100

    id_steam + exemple Stardew Valley

--- bdd_jeuxvideos.py
+++ bdd_jeuxvideos.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 # base de données de jeux vidéos
 #
 # contient les informations suivantes :
@@ -14,7 +16,15 @@ BDD = sql.connect("JEUXVIDEOS.db")
 curseur = BDD.cursor()
 
 # création d'une table JEUX
-curseur.execute("CREATE TABLE Jeux (nom TEXT PRIMARY KEY, date_sortie TEXT NOT NULL, genre TEXT NOT NULL, lien_steam TEXT NOT NULL);")
+curseur.execute("CREATE TABLE Jeux (id_steam TEXT PRIMARY KEY, nom TEXT NOT NULL, date_sortie TEXT NOT NULL, genre TEXT NOT NULL, lien_steam TEXT NOT NULL);")
+
+# on ajoute une instance 
+curseur.execute("INSERT INTO Jeux (id_steam, nom, date_sortie, genre, lien_steam) VALUES (\"413150\", \"Stardew Valley\", \"26 février 2016\", \"farming\", \"https://store.steampowered.com/app/413150/Stardew_Valley/\")")
+
+# on affiche toute la base de données
+curseur.execute("SELECT nom, date_sortie, genre FROM Jeux")
+result = curseur.fetchall()
+print(result)
 
 BDD.commit()
 BDD.close()
