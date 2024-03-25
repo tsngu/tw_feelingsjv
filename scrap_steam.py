8112591427c19d469724fcc6aa33bb25f7ab128f
tsngu <tifannys.nguyen@gmail.com>
2024-03-25 13:24:18 +0100

    ajout urls

--- scrap_steam.py
+++ scrap_steam.py
@@ -1,8 +1,8 @@
 import re
 import requests
-import os  
-import pandas as pd
+import os
 from bs4 import BeautifulSoup
+import pandas as pd
 import numpy as np
 
 # Fonction pour extraire les chiffres entre /app/ et /titre_du_jeu/
@@ -14,22 +14,38 @@ def extract_id(url):
     else:
         return None
 
-def extraire_avis(apps_id):
-    for app_id in apps_id:    
-        # Effectuer la requête avec l'ID d'application
-        response = requests.get(f'https://store.steampowered.com/appreviews/{app_id}?json=1').json()
+def extraire_n_avis(app_id, n=300):
+    reviews = []
+    cursor = '*'
+    params = {
+        'json': 1,
+        'filter': 'all',
+        'language': 'french',
+        'day_range': 9223372036854775807,
+        'review_type': 'all',
+        'purchase_type': 'all',
+        'cursor': cursor,
+        'num_per_page': 100
+    }
 
-        # Vérifier si la requête a réussi
-        if response.get('success') == 1:
-            # Récupérer les avis
-            reviews = response['reviews']
-            
-            # Écrire les avis dans un fichier
-            with open(f"./reviews/{app_id}.txt", "w", encoding="utf-8") as f:
-                for review in reviews:
-                    f.write(review['review'] + "\n")
-        else:
-            print(f"La requête pour l'app ID {app_id} a échoué.")
+    while n > 0:
+        params['cursor'] = cursor.encode()
+        params['num_per_page'] = min(100, n)
+
+        response = requests.get(f'https://store.steampowered.com/appreviews/{app_id}', params=params).json()
+
+        # Check if 'cursor' exists in the response
+        if 'cursor' not in response:
+            break
+
+        cursor = response['cursor']
+        reviews += response['reviews']
+        n -= 100
+
+        if len(response['reviews']) < 100:
+            break
+
+    return reviews
 
 # Read the URLs from file
 with open("urls_steam.txt", "r") as file:
@@ -38,5 +54,11 @@ with open("urls_steam.txt", "r") as file:
     # Extract app IDs from URLs
     apps_id = [extract_id(url) for url in urls if extract_id(url)]
 
-# Extract reviews for the games
-extraire_avis(apps_id)
+# Extract reviews for each game and write them to separate text files
+for app_id in apps_id:
+    reviews = extraire_n_avis(app_id)
+    
+    # Write reviews to a text file
+    with open(f"./reviews/{app_id}.txt", "w", encoding="utf-8") as f:
+        for review in reviews:
+            f.write(review['review'] + "\n\n#####\n\n")
